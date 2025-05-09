065f17f95fa7e4f1cf80942bf5c63826bb232217
sofyan <sofyanardiansyah888@gmail.com>
2025-05-09 16:15:12 +0800

    feat: add bank modules

--- models/bank.go
+++ models/bank.go
@@ -4,10 +4,18 @@ import (
 	"time"
 )
 
+type JenisBank string
+
+const (
+	BANK   JenisBank = "bank"
+	EMONEY JenisBank = "e-money"
+)
+
 type Bank struct {
 	ID        int       `gorm:"primary_key" json:"id"`
 	IDCabang  int64     `gorm:"not null" json:"id_cabang"`
 	NamaBank  string    `gorm:"string" json:"nama_bank"`
+	JenisBank JenisBank `gorm:"string" json:"jenis_bank"`
 	NoBank    string    `gorm:"unique" json:"no-bank" validate:"required"`
 	SaldoAwal float64   `gorm:"type:float;default:0" json:"saldo_awal"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
